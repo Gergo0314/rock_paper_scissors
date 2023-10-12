753a7ac89f3b55a7daebd1b76db076eecccb4166
Gergo <104260836+Gergo0314@users.noreply.github.com>
2023-10-12 17:33:03 +0200

    create playRound function instead of basic if statement

--- script.js
+++ script.js
@@ -9,23 +9,27 @@ function getComputerChoice() {
   return myList[index]; //returns a random item from myList
 }
 
-let playerInput = prompt("Choose your weapon (rock, paper, scissors)");
-let playerChoice = playerInput.toLowerCase(); //turns input into all lowercase
+const playerInput = prompt("Choose your weapon (rock, paper, scissors)");
+const playerSelection = playerInput.toLowerCase(); //turns input into all lowercase
+const computerSelection = getComputerChoice();
 
-if (playerChoice === getComputerChoice()) {
-  alert("It's a tie!");
-} else if (playerChoice === "rock" && getComputerChoice() === "paper") {
-  alert("You lose! Paper beats rock");
-} else if (playerChoice === "rock" && getComputerChoice() === "scissors") {
-  alert("You win! Rock beats scissors");
-} else if (playerChoice === "scissors" && getComputerChoice() === "rock") {
-  alert("You lose! Rock beats scissors");
-} else if (playerChoice === "scissors" && getComputerChoice() === "paper") {
-  alert("You win! Scissors beats paper");
-} else if (playerChoice === "paper" && getComputerChoice() === "scissors") {
-  alert("You lose! Scissors beats paper");
-} else if (playerChoice === "paper" && getComputerChoice() === "rock") {
-  alert("You win! Paper beats rock");
-} else {
-  alert("You mistyped that, buckaroo!");
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    alert("It's a tie!");
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    alert("You lose! Paper beats rock");
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    alert("You win! Rock beats scissors");
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    alert("You lose! Rock beats scissors");
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    alert("You win! Scissors beats paper");
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    alert("You lose! Scissors beats paper");
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    alert("You win! Paper beats rock");
+  } else {
+    alert("You mistyped that, buckaroo!");
+  }
 }
+playRound(playerSelection, computerSelection)
