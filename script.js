2bc637de9130c86ba631c8712ba2be09207e8fe2
Gergo <104260836+Gergo0314@users.noreply.github.com>
2023-10-12 17:01:16 +0200

    Create a one round rock paper scissors game. Input is case insensitive.

--- script.js
+++ script.js
@@ -8,4 +8,31 @@ function getComputerChoice() {
   const index = randNum(myList.length); //index = a random number between 0 and 2 (plugs 3 into randNum)
   return myList[index]; //returns a random item from myList
 }
-console.log(getComputerChoice());
+
+let playerInput = prompt("Choose your weapon (rock, paper, scissors)");
+let playerChoice = playerInput.toLowerCase() //turns input into all lowercase
+
+if (playerChoice === getComputerChoice()) {
+    alert("It's a tie!");
+}
+else if (playerChoice === "rock" && getComputerChoice() === "paper") {
+    alert("You lose! Paper beats rock");
+}
+else if (playerChoice === "rock" && getComputerChoice() === "scissors") {
+    alert("You win! Rock beats scissors");
+}
+else if (playerChoice === "scissors" && getComputerChoice() === "rock") {
+    alert("You lose! Rock beats scissors");
+}
+else if (playerChoice === "scissors" && getComputerChoice() === "paper") {
+    alert("You win! Scissors beats paper");
+}
+else if (playerChoice === "paper" && getComputerChoice() === "scissors") {
+    alert("You lose! Scissors beats paper");
+}
+else if (playerChoice === "paper" && getComputerChoice() === "rock") {
+    alert("You win! Paper beats rock");
+}
+else {
+    alert("You mistyped that, buckaroo!")
+}
