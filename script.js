aa391d1e5e0d5a10fcdea1d80416b79b1fed747b
Gergo <104260836+Gergo0314@users.noreply.github.com>
2023-10-13 15:56:51 +0200

    Format script.js, add score counter (hard af). Now you can play 5 rounds of r/p/s and keep track of the score.

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function randNum(n) {
-  //generates a random number between 0 and n
-  return Math.floor(Math.random() * n);
+  return Math.floor(Math.random() * n); //generates a random number between 0 and n
 }
 
 function getComputerChoice() {
@@ -12,31 +11,59 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
     alert("It's a tie!");
+    return "tie";
   } else if (playerSelection === "rock" && computerSelection === "paper") {
     alert("You lose! Paper beats rock");
+    return "computer";
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
     alert("You win! Rock beats scissors");
+    return "player";
   } else if (playerSelection === "scissors" && computerSelection === "rock") {
     alert("You lose! Rock beats scissors");
+    return "computer";
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
     alert("You win! Scissors beats paper");
+    return "player";
   } else if (playerSelection === "paper" && computerSelection === "scissors") {
     alert("You lose! Scissors beats paper");
+    return "computer";
   } else if (playerSelection === "paper" && computerSelection === "rock") {
     alert("You win! Paper beats rock");
+    return "player";
   } else {
     alert("You mistyped that, buckaroo!");
   }
 }
 
+var playerScore = 0;
+var computerScore = 0;
+
 function game() {
   for (i = 0; i <= 4; i++) {
-    randNum();
-    getComputerChoice();
     const playerInput = prompt("Choose your weapon (rock, paper, scissors)");
     const playerSelection = playerInput.toLowerCase(); //turns input into all lowercase
     const computerSelection = getComputerChoice();
-    playRound(playerSelection, computerSelection);
+    winner = playRound(playerSelection, computerSelection);
+    switch (winner) {
+      case "player": // player
+        playerScore++
+        break
+      case "computer": // cpu win
+        computerScore++
+        break
+      default: //tie
+        playerScore++
+        computerScore++
+    }
+    alert("Player: " + playerScore + " / " + "Computer: " + computerScore);
   }
 }
 game();
+
+if (playerScore === computerScore) {
+  alert("No one wins this game. Play another 5 round to prove your worth!");
+} else if ( playerScore > computerScore) {
+  alert("Congratulations, you win the game!");
+} else {
+  alert("Pfff, looooser! You lost...");
+}
